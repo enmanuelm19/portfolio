1c1be96be35ec844fad8e0020100e7808575ad32
enmanuel <enmanuel@upsocl.com>
2018-05-18 10:53:54 -0400

    Configuring devise

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,3 @@
+class Admin::PostsController < Admin::AdminController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'admin/posts#index', as: :root
+    end
+
+    unauthenticated do
+      root 'posts#new', as: :unauthenticated_root
+    end
+  end
+
+  namespace :admin do
+    resources :posts, only: [:index]
+  end
+
   resources :posts, only: [:index, :show]
 end
