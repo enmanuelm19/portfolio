5b025f03ee0558695047eced79b4766c3b049baf
enmanuel <enmanuel@upsocl.com>
2018-05-23 15:41:31 -0400

    Changes in js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,11 @@
 //= require_tree .
 
 function closeNotification(){
-  let element = document.getElementById('success_notification');
-  element.remove(element.selectedIndex);
+  var notification = document.getElementById('success_notification');
+  notification.remove(notification.selectedIndex);
 }
 
 function closeErro(){
-  let element = document.getElementById('error_notification');
-  element.remove(element.selectedIndex);
+  var notification = document.getElementById('error_notification');
+  notification.remove(notification.selectedIndex);
 }
