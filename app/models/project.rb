5c0d2fc083c7614dff37abb292d6fe6b8e12821c
Enmanuel Medina <enmanuelm19@gmail.com>
2018-06-10 18:08:39 -0400

    Adding header image for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
 class Project < ApplicationRecord
   paginates_per 10
   belongs_to :user
+  has_one_attached :header_image
   validates :title, :user, presence: true
 
   scope :published_projects, -> { where(published: true) }
-  scope :unpublished,-> { where.not(published: true) }
+  scope :unpublished, -> { where.not(published: true) }
 end
