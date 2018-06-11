527d2f5f3db08544478c90f3c73617e176b0508d
enmanuel <enmanuel@upsocl.com>
2018-06-11 17:08:14 -0400

    adding locale to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_184050) do
+ActiveRecord::Schema.define(version: 2018_06_11_205137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_184050) do
     t.bigint "user_id"
     t.boolean "published", default: false
     t.string "slug"
+    t.integer "locale", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
