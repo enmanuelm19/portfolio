3adc13a9ed3f5f6220c8f40547d8a2fe1d83102c
enmanuel <enmanuel@upsocl.com>
2018-06-25 11:28:49 -0400

    Adding some SEO improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  BRAND_NAME = 'Enmanuel'.freeze
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -15,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def locale_param 
     %w[es en].include?(params[:locale]) ? params[:locale] : 'es'
+  end
+
+  def meta_title(title)
+    [title, BRAND_NAME].reject(&:empty?).join(' | ')
   end
 end
