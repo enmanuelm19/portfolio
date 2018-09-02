c6b127e117d1f513d5240b5cab63e7ea7c62d51a
Enmanuel Medina <enmanuelm19@gmail.com>
2018-09-02 12:05:42 -0300

    [Fix] - Fixing locale problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /es|en/, defaults: {locale: 'es'} do
+  scope "(:locale)", locale: /es|en/ do
     get 'home/index'
     post 'contact-me', to: 'messages#create', as: 'create_message'
     devise_for :users, skip: :registration
