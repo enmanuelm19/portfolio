c87aaa08b17b6341de67ecbc9c8d968dbdf739ff
Enmanuel Medina <enmanuelm19@gmail.com>
2020-07-15 11:59:40 -0400

    [hotfix/v1.0.2] - Added metadata to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
+  before_action :set_og_properties
+
   def index
     @message =  Message.new
   end
+
+  private
+
+    def set_og_properties
+      @og_properties = {
+        title: @meta_title || ApplicationController::BRAND_NAME,
+        type: 'website',
+        image: 'https://i.imgur.com/kNvim2y.jpg',
+        url: 'https://enmanuelmedina.com'
+      }
+    end
 end
